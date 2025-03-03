d14c938760fc3d1d7296214be7a8aa85bce6ada0
JoseM-Castellano <josemariocastellano@gmail.com>
2025-03-03 20:01:24 +0100

    Hotfix mongo client

--- backend/src/connection/mongo_client.py
+++ backend/src/connection/mongo_client.py
@@ -1,5 +1,5 @@
 import pymongo
 
-conn = pymongo.MongoClient("mongodb://172.17.0.2:27017/")
+conn = pymongo.MongoClient("mongodb://localhost:27017/")
 db = conn["peliculasdb"]
 peliculascol = db["peliculas"]
